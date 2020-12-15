94b70c51999b966036852e0af585fdea4faf8d57
DanielAristy <daaristi@bancolombia.com.co>
2020-12-15 17:44:40 -0500

    Ciclos y manejo de callBacks

--- prototipos.js
+++ prototipos.js
@@ -1,31 +1,44 @@
-function heredaDe(claseHija, clasePadre){
-    var fn = function () {}
-    fn.prototype = clasePadre.prototype
-    claseHija.prototype = new fn
-    claseHija.prototype.constructor = claseHija 
+class Persona {
+    constructor(nombre, apellido, altura){
+        this.nombre = nombre
+        this.apellido = apellido
+        this.altura = altura
+    }
+    saludar(fn){
+        // var nombre = this.nombre
+        // var apellido = this.apellido
+        var { nombre, apellido } =  this
+        console.log(`Hola mi nombre es ${nombre} ${apellido}`)
+        if(fn){
+            fn(nombre, apellido)
+        }
+    }
+
+    soyAlto(){
+        return this.altura >=  1.70
+    }
 }
 
-function Persona(nombre, apellido, altura){
-    this.nombre = nombre
-    this.apellido = apellido
-    this.altura = altura
-}
-Persona.prototype.saludar = function (){
-    console.log(`Hola mi nombre es ${this.nombre} ${this.apellido}`)
-}
-
-function Desarrollador(nombre, apellido, altura){
-    this.nombre = nombre
-    this.apellido = apellido
-}
-heredaDe(Desarrollador,Persona)
-
-Desarrollador.prototype.saludar = function (){
-    console.log(`Hola, me lamo ${this.nombre} ${this.apellido}, y soy Desarrollador/a`)
+class Desarrollador extends Persona  {
+    constructor (nombre, apellido, altura){
+        super(nombre,apellido, altura)
+    }
+    saludar(fn){
+        var { nombre, apellido } =  this
+        console.log(`Hola, me lamo ${nombre} ${apellido}, y soy Desarrollador/a`)
+        if(fn){
+            fn(nombre, apellido, true)
+        }
+    }
 }
 
+function responderSaludo(nombre, apellido, esDev){
+    console.log(`Buen día ${nombre} ${apellido}`)
 
-
+    if(esDev){
+        console.log(`No sabía que eras desarrollador/a `)
+    }
+}
 /**Arrow function
  * Estas funciones no son muy recomendables 
  * con prototipos ya que como vemos en el 
@@ -36,10 +49,10 @@ Desarrollador.prototype.saludar = function (){
  */
 // Persona.prototype.soyAlto = () => this.altura >=  1.70
 
-Persona.prototype.soyAlto = function(){
-    return this.altura >=  1.70
-}
-
 var daniel = new Desarrollador('Daniel', 'Gomez', 1.80)
 var jose = new Persona('Jose', 'Ramirez', 1.65)
 var erick = new Persona('Erick', 'Hedai', 1.70)
+
+daniel.saludar(responderSaludo)
+jose.saludar(responderSaludo)
+erick.saludar(responderSaludo)
