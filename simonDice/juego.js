aeda72ad3134df458cdefe072f0df7dbaf051c23
DanielAristy <daaristi@bancolombia.com.co>
2020-12-16 14:26:39 -0500

    Eventos onClick para seleccionar los colores

--- simonDice/juego.js
+++ simonDice/juego.js
@@ -12,8 +12,9 @@ class Juego {
     }
 
     inicializar() {
+        this.elegirColor = this.elegirColor.bind(this)
         btnEmpezar.classList.add("hide");
-        this.nivel = 2;
+        this.nivel = 1;
         this.colores = {
             celeste, violeta, naranja, verde
         }
@@ -23,23 +24,12 @@ class Juego {
     }
     siguienteNivel(){
         this.iluminarSecuencia();
+        this.agregarEventoClick();
     }
 
-    transformarNumeroAColor(numero){
-        switch(numero){
-            case 0:
-                return 'celeste'
-            case 1:
-                return 'violeta'
-            case 2:
-                return 'naranja'
-            case 3:
-                return 'verde'
-        }
-    }
     iluminarSecuencia(){
         for (let i = 0; i < this.nivel; i++) {
-            const color = this.transformarNumeroAColor(this.secuencia[i])
+            const color = this.transformarColorANumero(this.secuencia[i])
             setTimeout(() => this.iluminarColor(color), 1000 * i);
         }
     }
@@ -50,6 +40,33 @@ class Juego {
     apagarColor(color){
         this.colores[color].classList.remove('light');
     }
+    
+    agregarEventoClick(){
+        this.colores.celeste.addEventListener('click', this.elegirColor);
+        this.colores.violeta.addEventListener('click', this.elegirColor);
+        this.colores.naranja.addEventListener('click', this.elegirColor);
+        this.colores.verde.addEventListener('click', this.elegirColor);
+    }
+
+    elegirColor(en){
+        const nombreColor = en.target.dataset.color;
+        const idColor = this.transformarColorANumero(nombreColor)
+        this.iluminarColor(nombreColor)
+    }
+
+    transformarColorANumero(color){
+        switch(color){
+            case 'celeste':
+                return 0
+            case 'violeta':
+                return 1
+            case 'naranja':
+                return 2
+            case 'verde':
+                return 3
+        }
+    }
+    
 }
 
 
