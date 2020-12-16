4cb25a9bf0613da4cfb03a14d84a0f29eed4f27f
DanielAristy <daaristi@bancolombia.com.co>
2020-12-16 10:07:51 -0500

    Obtener personajes dentro de un array

--- callback.js
+++ callback.js
@@ -20,8 +20,16 @@ function obtenerPersonaje(id){
 function onError(id){
     console.log(`Sucedió al obtener el personaje ${id}`)
 }
-obtenerPersonaje(1)
-    .then(function(personaje) {
-        console.log(`El personaje 1 es ${personaje.name}`)
-    })
+
+var ids = [1,2,3,4,5,6];
+
+var promesas = ids.map(id => obtenerPersonaje(id));
+
+Promise.all(promesas)
+    .then(personajes => console.log(personajes))
     .catch(onError)
+// obtenerPersonaje(1)
+//     .then(personaje => {
+//         console.log(`El personaje 1 es ${personaje.name}`)
+//     })
+//     .catch(onError)
